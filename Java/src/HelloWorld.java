55a865854533d03783d5a5f801625e0cba18a5ab
soonsuyeon <soocy0718@gmail.com>
2023-02-23 13:43:04 +0900

    Update 23.02.23

---
+++ Java/src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!!");
+    }
+}
+// source, code, language
+
+
+// Hello World!! 결과 : application, program
+
+// computer / <- Run / Java Virtual Machine / <- Run / Java Application .class / <- compile / Java Source code .java
