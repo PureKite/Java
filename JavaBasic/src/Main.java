9e8c0445929b53495832fdc7e165e9036cc6738b
soonsuyeon <soocy0718@gmail.com>
2023-07-03 20:08:20 +0900

    Update 23.07.03

--- JavaBasic/src/Main.java
+++ JavaBasic/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Solution s = new Solution();
+        String[] a = {"classic", "pop", "classic", "classic", "pop"};
+        int[] b = {500, 600, 150, 800, 2500};
+        s.solution(a, b);
     }
 }
